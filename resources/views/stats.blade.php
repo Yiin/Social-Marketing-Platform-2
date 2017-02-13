9fe29f4600b10d8cd68b3198b057a3b9b4dfa145
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2017-02-13 09:47:46 +0200

    stats view

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -6,32 +6,31 @@
             <div class="col-xs-8 col-xs-offset-2">
                 <div class="card">
                     <div class="header text-center">
-                        Log. Posted to {{ $queue->statistic_groups }} groups,
-                        ~ {{ number_format($queue->statistic_members) }}
+                        Log. Posted {{ $queue->stats['posts'] }} posts,
+                        ~ {{ number_format($queue->stats['backlinks']) }}
                         backlinks
                     </div>
                     <div class="content">
-                        @foreach($queue->log()->orderBy('id', 'desc')->get() as $log)
-                            <div>
-                                @if($log->log->type === 'log')
-                                    <h4 class="title">Posting to "{{ $log->log->communityName }}"</h4>
-                                    <div class="content">
-                                        @if($queue->posts()->where('group', $log->log->communityId)->exists())
-                                            Posts:
-                                        @endif
-                                        <ul>
-                                            @foreach($queue->posts()->where('group', $log->log->communityId)->get() as $post)
-                                                <li>
-                                                    <a href="{{ $post->url }}" target="_blank">{{ $post->message }}</a>
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                @elseif ($log->log->type === 'error')
-                                    <label class="error">{{ $log->log->error_message }}</label>
-                                @endif
-                            </div>
-                        @endforeach
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Time</th>
+                                <th>Group Name</th>
+                                <th>Message</th>
+                                <th>URL</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($queue->posts()->orderBy('id', 'desc')->get() as $post)
+                                <tr>
+                                    <td>{{ $post->created_at->diffForHumans() }}</td>
+                                    <td>{{ $post->data['groupName'] }}</td>
+                                    <td>{{ $post->data['message'] }}</td>
+                                    <td>{{ $post->data['url'] }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
