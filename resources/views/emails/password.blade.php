7f403ed7b578eed2a6b1cb9146465e80e9e7a745
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2017-04-10 12:19:29 +0300

    welcome email fix

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,12 +1,12 @@
 @component('mail::message')
-# You can now login to Social Media Panel using details below
+    # You can now login to Social Media Panel using details below
 
-Login name: $email <br/>
-Your password: $password
+    Login name: {{ $email }}<br/>
+    Your password: {{ $password }}
 
-@component('mail::button', ['url' => URL::to(route('login'))])
-Login!
-@endcomponent
+    @component('mail::button', ['url' => URL::to(route('login'))])
+        Login!
+    @endcomponent
 
-{{ config('app.name') }}
+    {{ config('app.name') }}
 @endcomponent
