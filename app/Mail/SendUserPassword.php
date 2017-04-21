bea3ff092bf78f6c6d467f572b2fca7020f78279
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2017-04-21 19:55:05 +0300

    mail html fix

--- app/Mail/SendUserPassword.php
+++ app/Mail/SendUserPassword.php
@@ -32,6 +32,6 @@ class SendUserPassword extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.password');
+        return $this->view('emails.password');
     }
 }
