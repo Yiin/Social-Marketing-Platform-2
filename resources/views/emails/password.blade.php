e154511c2c485fdb99dd0bc456c432c4cb133136
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2017-04-27 02:13:53 +0300

    do not render email content as code blocks

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,13 +1,17 @@
+{{-- Do not use excess indentation when writing Markdown emails. Markdown parsers will render indented content as code blocks. --}}
+
 @component('mail::message')
-    # You can now login to Social Media Panel using details below
 
-    Login name: {{ $email }}
+# You can now login to Social Media Panel using details below
 
-    Your password: {{ $password }}
+Login name: {{ $email }}
 
-    @component('mail::button', ['url' => URL::to(route('login'))])
-        Login!
-    @endcomponent
+Your password: {{ $password }}
 
-    {{ config('app.name') }}
+@component('mail::button', ['url' => URL::to(route('login'))])
+    Login!
+@endcomponent
+
+{{ config('app.name') }}
+
 @endcomponent
